b4c0cc188501930c0fc21a45b6f8ab6ae01f729a
edwinj85 <edwin_jones@hotmail.co.uk>
2014-07-21 22:09:44 +0100

    #first #1 first commit of echo

---
+++ Echo/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Xml.Linq;
+
+namespace Echo
+{  
+    static class Program
+    {
+        static void Main()
+        {
+            bool SystemsNominal = true;
+            bool networkOnline = true;
+
+            string temperature = "unavailable";
+            string conditions = "unavailable";
+
+            //attempt to get weather information from Wunderground api.
+            try
+            {
+                //WUNDERGROUND API KEY (500 requests per day, 10 per minute) = cd90a090e8086362
+                XDocument xml = XDocument.Load("http://api.wunderground.com/api/cd90a090e8086362/hourly/q/autoip.xml");
+
+                //locate the most recent weather report
+                var forecast = xml.Descendants("forecast").First();
+
+                //get the temperature and conditions out of the report
+                temperature = forecast.Descendants("temp").First().Descendants("metric").First().Value;
+                conditions = forecast.Descendants("condition").First().Value;
+            }
+            catch (WebException) //error, we cant be fully online or the weather api is down/has changed!
+            {
+                networkOnline = false;
+            }
+            catch (Exception)
+            {
+
+            }
+
+            //get system stats
+            var cpuSpeed = (int)SystemMonitor.Instance.GetCPU0Speed();
+            var cpuTemp = (int)SystemMonitor.Instance.GetCPU0Temp();
+            var gpuSpeed = (int)SystemMonitor.Instance.GetGPU0Speed();
+            var gpuTemp = (int)SystemMonitor.Instance.GetGPU0Temp();
+
+            //warn user if systems are not nominal.
+            if(cpuSpeed < 3000 
+                || cpuSpeed > 5000
+                || cpuTemp < 10
+                || cpuTemp > 70
+                || !networkOnline)
+            {
+                SystemsNominal = false;
+            }
+
+            //Set up speech generator
+            System.Speech.Synthesis.SpeechSynthesizer voice = new System.Speech.Synthesis.SpeechSynthesizer();
+            voice.SetOutputToDefaultAudioDevice();
+
+            //speak basic information about computer
+            voice.Speak("Greetings " + Environment.UserName);
+            voice.Speak("Network connection, " + (networkOnline ? "Online" : "Offline"));
+            voice.Speak("Core CPU Temperature, " + cpuTemp + " Degrees.");
+            voice.Speak("Core CPU Speed, " + cpuSpeed  + " Megahertz.");
+            voice.Speak("Core GPU Temperature, " + gpuTemp + " Degrees.");
+            voice.Speak("Core GPU Speed, " + gpuSpeed + " Megahertz.");
+            voice.Speak(temperature != string.Empty ? "Ambient temperature, " + temperature + " Degrees." : string.Empty);
+            voice.Speak(conditions != string.Empty ? "The ambient weather conditions are, " + conditions : string.Empty);
+            voice.Speak("The date is " + DateTime.Now.ToString("dddd, MMMM dd, yyyy"));
+            voice.Speak("Local time is, " + DateTime.Now.ToString("HH,mm"));
+            voice.Speak(SystemsNominal ? "All systems nominal" : "Error in startup procedure, systems are not nominal");       
+        }
+    }
+}
---
+++ Echo/SystemMonitor.cs
@@ -0,0 +1,209 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenHardwareMonitor;
+using OpenHardwareMonitor.Hardware;
+
+namespace Echo
+{
+    public class SystemMonitor
+    {
+
+        public static SystemMonitor Instance = new SystemMonitor();
+
+        private SystemMonitor()
+        {
+            myComputer.Open();
+        }
+
+        static MySettings settings = new MySettings(new Dictionary<string, string>
+            {
+                { "/intelcpu/0/temperature/0/values", "H4sIAAAAAAAEAOy9B2AcSZYlJi9tynt/SvVK1+B0oQiAYBMk2JBAEOzBiM3mkuwdaUcjKasqgcplVmVdZhZAzO2dvPfee++999577733ujudTif33/8/XGZkAWz2zkrayZ4hgKrIHz9+fB8/Iu6//MH37x79i9/+NX6N3/TJm9/5f/01fw1+fosnv+A/+OlfS37/jZ/s/Lpv9fff6Ml/NTef/yZPnozc5679b+i193//TQZ+/w2Dd+P9/sZeX/67v/GTf/b3iP3u4/ObBL//73+i+f039+D8Zk/+xz/e/P6beu2TQZju8yH8f6OgzcvPv/U3/Rb8+z/0f/9b/+yfaOn8079X6fr6Cws7ln/iHzNwflPv99/wyS/+xY4+v/evcJ+733+jJ5//Cw7/4ndy9Im3+U2e/Fbnrk31C93vrt/fyPvdb+N//hsF7/4/AQAA//9NLZZ8WAIAAA==" }
+            });
+
+        Computer myComputer = new Computer(settings)
+        {
+            MainboardEnabled = true,
+            CPUEnabled = true,
+            RAMEnabled = true,
+            GPUEnabled = true,
+            FanControllerEnabled = true,
+            HDDEnabled = true
+        };
+
+        public Single GetCPU0Speed()
+        {
+            Single result = 0f;
+
+            foreach (var hardwareItem in myComputer.Hardware)
+            {
+                if (hardwareItem.HardwareType == HardwareType.CPU)
+                {
+                    hardwareItem.Update();
+                    foreach (IHardware subHardware in hardwareItem.SubHardware)
+                    {
+                        subHardware.Update();
+                    }
+
+                    foreach (var sensor in hardwareItem.Sensors)
+                    {
+                        if (sensor.SensorType == SensorType.Clock)
+                        {
+                            result = (float)sensor.Value;
+                            return result;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+
+
+
+
+        public Single GetCPU0Temp()
+        {
+            Single result = 0f;
+
+            foreach (var hardwareItem in myComputer.Hardware)
+            {
+                if (hardwareItem.HardwareType == HardwareType.CPU)
+                {
+                    hardwareItem.Update();
+                    foreach (IHardware subHardware in hardwareItem.SubHardware)
+                    {
+                        subHardware.Update();
+                    }
+
+                    foreach (var sensor in hardwareItem.Sensors)
+                    {
+                        if (sensor.SensorType == SensorType.Temperature)
+                        {
+                            result = (float)sensor.Value;
+                            return result;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public Single GetGPU0Speed()
+        {
+            Single result = 0f;
+
+            foreach (var hardwareItem in myComputer.Hardware)
+            {
+                if (hardwareItem.HardwareType == HardwareType.GpuAti || hardwareItem.HardwareType == HardwareType.GpuNvidia)
+                {
+                    hardwareItem.Update();
+                    foreach (IHardware subHardware in hardwareItem.SubHardware)
+                    {
+                        subHardware.Update();
+                    }
+
+                    foreach (var sensor in hardwareItem.Sensors)
+                    {
+                        if (sensor.SensorType == SensorType.Clock)
+                        {
+                            result = (float)sensor.Value;
+                            return result;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public Single GetGPU0Temp()
+        {
+            Single result = 0f;
+
+            foreach (var hardwareItem in myComputer.Hardware)
+            {
+                if (hardwareItem.HardwareType == HardwareType.GpuAti || hardwareItem.HardwareType == HardwareType.GpuNvidia)
+                {
+                    hardwareItem.Update();
+                    foreach (IHardware subHardware in hardwareItem.SubHardware)
+                    {
+                        subHardware.Update();
+                    }
+
+                    foreach (var sensor in hardwareItem.Sensors)
+                    {
+                        if (sensor.SensorType == SensorType.Temperature)
+                        {
+                            result = (float)sensor.Value;
+                            return result;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+
+    public class MySettings : ISettings
+    {
+        private IDictionary<string, string> settings = new Dictionary<string, string>();
+
+        public MySettings(IDictionary<string, string> settings)
+        {
+            this.settings = settings;
+        }
+
+        public bool Contains(string name)
+        {
+            return settings.ContainsKey(name);
+        }
+
+        public string GetValue(string name, string value)
+        {
+            string result;
+            if (settings.TryGetValue(name, out result))
+                return result;
+            else
+                return value;
+        }
+
+        public void Remove(string name)
+        {
+            settings.Remove(name);
+        }
+
+        public void SetValue(string name, string value)
+        {
+            settings[name] = value;
+        }
+    }
+
+    //public class OHW
+    //{
+    //    private static OHW m_Instance;
+    //    public static OHW Instance
+    //    {
+    //        get
+    //        {
+    //            if (m_Instance == null)
+    //                m_Instance = new OHW();
+
+    //            return m_Instance;
+    //        }
+    //    }
+    //    private OHW()
+    //    {
+    //        m_Instance = this;
+    //    }
+
+
+
+    //}
+}
